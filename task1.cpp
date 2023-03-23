b070e3923d77e5498eadf477dffa3e7f1690ee64
Toxiccoder111 <112707642+Toxiccoder111@users.noreply.github.com>
2023-03-23 21:35:30 +0530

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+int main()
+{
+    cout << "Name:Avinash Thakur";
+    cout << endl;
+    cout << "DOB:02/10/2002";
+    cout << endl;
+    cout << "Email:mr.avi9098thakur@gmail.com";
+    cout << endl;
+}
